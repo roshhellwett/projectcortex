2f9872af66f75fcd0ebce288582429d82a55b8cf
roshhellwett <roshhellwett@icloud.com>
2026-06-12 22:16:33 +0530

    style: replace default SVG logo with extension icon

--- content_ui.js
+++ content_ui.js
@@ -208,7 +208,7 @@ function buildPanelHTML() {
   return `
     <div class="pm-header">
       <div class="pm-logo">
-        <svg viewBox="0 0 24 24" fill="none"><path d="M13 2L3 14h9l-1 8 10-12h-9l1-8z" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>
+        <img src="${chrome.runtime.getURL('icons/logo48.png')}" alt="ProjectCortex">
         <span class="pm-logo-text">Cortex</span>
         <span class="pm-badge">AI</span>
       </div>
